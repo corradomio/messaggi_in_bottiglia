edc9f95db6295184c68fbba8d3574e26a22e3f52
Corrado Mio <corrado.mio@gmail.com>
2017-03-12 16:34:24 +0100

    Update Synonimous implementation

--- test_synon.py
+++ test_synon.py
@@ -4,16 +4,31 @@ import orientdb as o
 
 
 def main():
+    odb = o.OrientDB("orient://root:password@cmshooter.homeip.net:2424/test")
+    odb.open_db()
+
+    odb.create_class_vertex("Word", {"text": str})
+    odb.create_class_edge("Synonimous")
+
+    print(odb.list_classes())
+
     syn = t.Synonimous()
     syn.load_synonimous("topics/Italiano/th_it_IT_v2.txt")
 
-    odb = o.OrientDB
-
     for w in syn:
         print(w)
+
+        vw = odb.select_vertex("Word", where="text=${text}", params={"text": w})
+        if vw is None:
+            vw = odb.insert_vertex("Word", body={"text": w})
+
         slist = syn[w]
         for s in slist:
             print("... %s" % s)
+
+    odb.close()
+
+
 #end
 
 
